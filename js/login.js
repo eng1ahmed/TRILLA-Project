a9cb1d4c91897a906a714f38e40c83bd83aa0eea
eng1ahmed <engahmed11132@gmail.com>
2024-12-12 12:06:53 +0200

    edp

--- js/login.js
+++ js/login.js
@@ -1,17 +1,24 @@
-let loginForm = document.getElementById("loginForm");
-let userName = document.getElementById("username");
-let Password = document.getElementById("password");
-let btn = document.querySelector("button")
-// userName.onblur = function(){
-//     sessionStorage.userName = userName.value;
-//     sessionStorage.Password = userName.value;
+// let lf = document.querySelector("#loginForm");
+// let userName = document.getElementById("username");
+// let Password = document.getElementById("password");
+// let btn = document.querySelector("button");
+// function click (){
+//           window.location.pathname = "./html/page.html";
 // }
 
-loginForm.onsubmit = function(){
-    localStorage.userName = userName.value;
-    localStorage.password = Password.value;
-}
-console.log(window.location.pathname);
-btn.onclick = function(){
- window.location.pathname = "/TRILLA-Project/html/page.html";
-}
+// lf.onsubmit = function () {
+//   localStorage.userName = userName.value;
+//   localStorage.password = Password.value;
+//   click();
+// };
+const loginForm = document.getElementById("loginForm");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  localStorage.setItem("userName", usernameInput.value);
+  localStorage.setItem("password", passwordInput.value);
+
+  window.location.href = "./html/page.html";
+});
