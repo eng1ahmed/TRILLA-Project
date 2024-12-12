5f17a29aa632e2e123099a6f19bea5c3a37a1963
Ahmed Sayed <engahmed11132@gmail.com>
2024-12-12 10:43:54 +0200

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,17 @@
+let loginForm = document.getElementById("loginForm");
+let userName = document.getElementById("username");
+let Password = document.getElementById("password");
+let btn = document.querySelector("button")
+// userName.onblur = function(){
+//     sessionStorage.userName = userName.value;
+//     sessionStorage.Password = userName.value;
+// }
+
+loginForm.onsubmit = function(){
+    localStorage.userName = userName.value;
+    localStorage.password = Password.value;
+}
+console.log(window.location.pathname);
+btn.onclick = function(){
+ window.location.pathname = "/html/page.html";
+}
---
+++ js/page.js
@@ -0,0 +1,38 @@
+let wellcome = document.querySelector("#wellcome");
+
+wellcome.innerHTML = `WellCome ${localStorage
+  .getItem("userName")
+  .toUpperCase()}`;
+
+class addProduct {
+  constructor(productName, price, description) {
+    let i;
+    let allproducts = document.getElementById("allp");
+    let product = document.createElement("div");
+    let content = document.createElement("div");
+    content.id = "content";
+    product.classList.add("product");
+    product.id = `product ${i}`;
+    allproducts.appendChild(product);
+    let img = document.createElement("img");
+    product.appendChild(img);
+    product.appendChild(content);
+    img.src =
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSap-eSqTTO2iEqPYAvkSQjYLCxmDNeG7WRg&s";
+    let name = document.createElement("h2");
+    name.innerText = `Car Name :${productName}`;
+    content.appendChild(name);
+    let p = document.createElement("h3");
+    p.innerText = `Price : ${price} $`;
+    p.id = "price"
+    content.appendChild(p);
+    let descrip = document.createElement("h3");
+    descrip.innerText = `Description : ${description}`;
+    content.appendChild(descrip);
+    i++;
+  }
+}
+
+let product1 = new addProduct("Car 1", 200, "i have good car");
+let product2 = new addProduct("Car 2", 500, "i have good car");
+let product3 = new addProduct("Car 3", 1000, "i have good car");
