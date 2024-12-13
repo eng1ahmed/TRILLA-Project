a1c340eab64c9f4cc68801d4ccddf2ce951919e4
eng1ahmed <engahmed11132@gmail.com>
2024-12-13 15:53:27 +0200

    e

--- js/signup.js
+++ js/signup.js
@@ -5,10 +5,17 @@ const passwordInput = document.getElementById("password");
 const phoneInput = document.getElementById("phone");
 
 // make action when submited
-loginForm.addEventListener("submit", (event) => {
+signUpForm.addEventListener("submit", (event) => {
   event.preventDefault();
-    localStorage.setItem("userName", usernameInput.value);
+  if(passwordInput.value.length <= 8){
+    alert("Please enter 8 charcter password")
+  }
+  else{
+        localStorage.setItem("userName", usernameInput.value);
     localStorage.setItem("password", passwordInput.value);
     localStorage.setItem("phone", phoneInput.value);
     window.location.href = "./html/index.html";
+  }
+
+
 });
